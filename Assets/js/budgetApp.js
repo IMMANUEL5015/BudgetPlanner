91428756231539f3f63f2d1460678d0e6cfd4cfc
IMMANUEL5015 <immanueldiai@gmail.com>
2019-11-06 08:10:08 +0100

    Calculate Expenses Feature

--- Assets/js/budgetApp.js
+++ Assets/js/budgetApp.js
@@ -12,29 +12,42 @@ $('#budget input[type="number"]').keypress(function(event){
 });
 
 //Enter Expenses Details
+var expensesSum = 0;
 var expenses = new Array();
 $('#expense button').click(function(e){
+    expensesSum = 0;
     var expenseName = $('#expense input[type="text"]').val();
     var expenseAmount = $('#expense input[type="number"]').val();
     if(expenseName !== '' && expenseAmount != 0){
         expenses.push({title: expenseName, amount: parseInt(expenseAmount)});
         var newRow = "<tr>" + "<td>" + expenseName + "</td>" + "<td>" + expenseAmount + "</td>" + "<td>" + "Edit" + "</td>" + "<td>" + "Delete" + "</td>" + "</tr>";
         $('table tbody').append(newRow);
+        $('#expense input[type="text"]').val('');  
+        $('#expense input[type="number"]').val('');
     }
-    $('#expense input[type="text"]').val('');  
-    $('#expense input[type="number"]').val('');
+    calcExpenses();
+    $('#secondReport #figure2').text(expensesSum);
     e.stopPropagation();
 });
 
-//Calculate Balance
-var balance;
-var expensesSum = 0;
-$('#calcBalance button').on("click", function(e){
-    expensesSum = 0;
+//Declare the function to Calculate Expenses
+function calcExpenses(){
     expenses.forEach(function(expense){
         expensesSum+=expense.amount;
     });
-    balance = parseInt(budgetAmount) - expensesSum;
-    $('#thirdReport #figure3').text(balance);
+}
+
+//Calculate Balance
+var balance;
+$('#calcBalance button').on("click", function(e){
+    expensesSum = 0;
+    calcExpenses();
+    calcBalance();
     e.stopPropagation();
 });
+
+//Declare the function to Calculate Balance
+function calcBalance(){
+    balance = parseInt(budgetAmount) - expensesSum;
+    $('#thirdReport #figure3').text(balance);
+}
